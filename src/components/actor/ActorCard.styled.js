18968bccfa691a2daf2bd8fff832c5dd748e8cdc
sonu saini <sonusaini6280@gmail.com>
2022-06-19 18:09:43 +0530

    installed styled components and created styles for search cards

---
+++ src/components/actor/ActorCard.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { SearchCard } from '../styled';
+
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 1;
+    margin-top: 15px;
+    padding: 10px;
+    font-weight: bold;
+  }
+`;
